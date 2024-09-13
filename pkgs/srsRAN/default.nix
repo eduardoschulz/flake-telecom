01589a4ad105f7837708ff927be9f28ece1d2e21
Eduardo Schulz <eduardo.schulz@protonmail.com>
2024-09-13 18:39:10 -0300

    teste

--- pkgs/srsRAN/default.nix
+++ pkgs/srsRAN/default.nix
@@ -25,6 +25,10 @@
 
 #(overrideCC stdenv gcc10).mkDerivation rec{
 #stdenvNoCC.mkDerivation rec {
+let 
+	pkgs = import <nixpkgs> {};
+in
+
 gcc12Stdenv.mkDerivation rec {
   pname = "srsRAN-Project";
   version = "24_04";
@@ -41,26 +45,39 @@ gcc12Stdenv.mkDerivation rec {
 		pkg-config
   ];
 	
-	buildInputs = [
-    fftwFloat
-    mbedtls
-    boost
-    libconfig
-    lksctp-tools
-    pcsclite
-    uhd
-    soapysdr-with-plugins
-    libbladeRF
-    zeromq
-		gtest
-		yaml-cpp
-		libbfd
-		libdwg
-		libdwarf
-		doxygen
-		gcc10
-  ];
-	#"-DENABLE_WERROR=OFF" "-DENABLE_ZEROMQ=TRUE" "-DENABLE_EXPORT=TRUE"
+
+buildInputs = [
+  fftwFloat
+  mbedtls
+  boost
+  libconfig
+  lksctp-tools
+  pcsclite
+  uhd
+  soapysdr-with-plugins
+  libbladeRF
+  zeromq
+  (pkgs.fetchFromGitHub {
+    owner = "google";
+    repo = "googletest";
+    rev = "release-1.12.1";  # Specify the version you need
+    sha256 = "1cv55x3amwrvfan9pr8dfnicwr8r6ar3yf6cg9v6nykd6m2v3qsv";  # Replace with actual sha256
+  })
+  yaml-cpp
+  libbfd
+  libdwg
+  libdwarf
+  doxygen
+  gcc10
+];
+
+
+
+
+
+
+
+		#"-DENABLE_WERROR=OFF" "-DENABLE_ZEROMQ=TRUE" "-DENABLE_EXPORT=TRUE"
 
 	cmakeFlags = [ "-DBUILD_TESTING=OFF"  ];
 
