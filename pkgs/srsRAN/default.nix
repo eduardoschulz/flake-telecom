0a76ac06083f9002c12a5f784455b58879f71482
Eduardo Schulz <eduardo.schulz@protonmail.com>
2024-09-13 20:07:00 -0300

    desisto por hj

--- pkgs/srsRAN/default.nix
+++ pkgs/srsRAN/default.nix
@@ -1,5 +1,5 @@
 { stdenv
-, gcc12Stdenv
+, gcc11Stdenv
 , lib
 , cmake
 , fetchFromGitHub
@@ -19,81 +19,105 @@
 , libdwg
 , libdwarf
 , doxygen
+, python3
 }:
 
 let
-  # Define the dependency first
-  dependency = stdenv.mkDerivation {
+  # Define googletest as a separate derivation
+  googletest = stdenv.mkDerivation {
+    pname = "googletest";
+    version = "1.12.1";
 
+    src = fetchFromGitHub {
+      owner = "google";
+      repo = "googletest";
+      rev = "release-1.12.1";
+      sha256 = "1cv55x3amwrvfan9pr8dfnicwr8r6ar3yf6cg9v6nykd6m2v3qsv";
+    };
 
-	googletest = fetchFromGitHub {
-    owner = "google";
-    repo = "googletest";
-    rev = "release-1.12.1";  # Ensure version and sha256 are correct
-    sha256 = "1cv55x3amwrvfan9pr8dfnicwr8r6ar3yf6cg9v6nykd6m2v3qsv";
-  	};
-	};
-main = gcc12Stdenv.mkDerivation rec {
-  pname = "srsRAN-Project";
-  version = "24_04";
+    nativeBuildInputs = [ cmake python3];
 
-  src = fetchFromGitHub {
-    owner = "srsran";
-    repo = "srsRAN_Project";
-    rev = "release_${version}";
-    hash = "sha256-ZgOeWpmcqQE7BYgiaVysnRkcJxD4fTCfKSQC5hIGTfk=";
+    buildPhase = ''
+      cmake ..
+      make
+			ls
+    '';
+
+    installPhase = ''
+			make install
+    '';
+
+      #mkdir -p $out/lib $out/include
+      #cp -r lib/ $out/lib
+
+    meta = {
+      description = "Google's C++ test framework";
+      homepage = "https://github.com/google/googletest";
+      license = lib.licenses.bsd3;
+      platforms = lib.platforms.all;
+    };
   };
 
+  # Define the main project derivation
+  main = gcc11Stdenv.mkDerivation rec {
+    pname = "srsRAN-Project";
+    version = "24_04";
 
-  nativeBuildInputs = [
-    cmake
-    pkg-config
-  ];
+    src = fetchFromGitHub {
+      owner = "srsran";
+      repo = "srsRAN_Project";
+      rev = "release_${version}";
+      sha256 = "ZgOeWpmcqQE7BYgiaVysnRkcJxD4fTCfKSQC5hIGTfk=";
+    };
 
-  buildInputs = [
-    fftwFloat
-    mbedtls
-    boost
-    libconfig
-    lksctp-tools
-    pcsclite
-    uhd
-    soapysdr-with-plugins
-    libbladeRF
-    zeromq
-    yaml-cpp
-    libbfd
-    libdwg
-    libdwarf
-    doxygen
-    googletest
-  ];
+    nativeBuildInputs = [
+      cmake
+      pkg-config
+    ];
 
-  
-  cmakeFlags = [ 
-		"-Wno-dev"
-		"-DGTEST_LIBRARY=${googletest}/lib"
-    "-DGTEST_MAIN_LIBRARY=${googletest}/lib"
-    "-DGTEST_INCLUDE_DIR=${googletest}/include"
-	];
-	
-	buildPhase = ''
-	'';
-	installPhase = ''
-		cd $googletest/googletest
-		mkdir -p build
-		cd build 
-		cmake ..
-	'';
+    buildInputs = [
+      fftwFloat
+      mbedtls
+      boost
+      libconfig
+      lksctp-tools
+      pcsclite
+      uhd
+      soapysdr-with-plugins
+      libbladeRF
+      zeromq
+      yaml-cpp
+      libbfd
+      libdwg
+      libdwarf
+      doxygen
+      googletest  # Now we are using googletest as a dependency
+    ];
 
-  meta = {
-    description = "Open source O-RAN 5G CU/DU solution from Software Radio Systems (SRS)";
-    homepage = "https://github.com/srsran/srsRAN_Project";
-    changelog = "https://github.com/srsran/srsRAN_Project/blob/${version}/CHANGELOG";
-    license = lib.licenses.agpl3Only;
-    maintainers = with lib.maintainers; [];
-    platforms = lib.platforms.all;
+    cmakeFlags = [
+      "-Wno-dev"
+			"-Wfatal-errors"
+     # "-DGTEST_LIBRARY=${googletest}/lib/libgtest.a"
+     # "-DGTEST_MAIN_LIBRARY=${googletest}/lib/libgtest_main.a"
+     # "-DGTEST_INCLUDE_DIR=${googletest}/include"
+    ];
+
+    buildPhase = ''
+      cmake -Wno-error ..
+      make -j22
+    '';
+
+
+
+    meta = {
+      description = "Open source O-RAN 5G CU/DU solution from Software Radio Systems (SRS)";
+      homepage = "https://github.com/srsran/srsRAN_Project";
+      changelog = "https://github.com/srsran/srsRAN_Project/blob/${version}/CHANGELOG";
+      license = lib.licenses.agpl3Only;
+      maintainers = with lib.maintainers; [];
+      platforms = lib.platforms.all;
+    };
   };
-};
-}
+in
+  main
 
