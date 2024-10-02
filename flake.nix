15a72c3d3f1a395bbd05f28b94df72d7097e0369
Eduardo Schulz <eduardo.schulz@protonmail.com>
2024-10-02 17:23:07 -0300

    add getenv function to flake

--- flake.nix
+++ flake.nix
@@ -3,42 +3,61 @@
 
   inputs = {
     # NixOS official package source, here using the nixos-23.11 branch
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05"; #"github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url =
+      "github:NixOS/nixpkgs/nixos-24.05"; # "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
-	url = "github:nix-community/home-manager/release-24.05";
-	inputs.nixpkgs.follows = "nixpkgs"; 
-		};
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     catppuccin.url = "github:catppuccin/nix";
   };
 
   outputs = { self, nixpkgs, home-manager, catppuccin, ... }:
-	let
-		system = "x86_64-linux"; #change this if other architecture
-		pkgs = import nixpkgs {
-			inherit system;
-			config.allowUnfree = true;
-		};
-      	lib = nixpkgs.lib;
-	in {
-		nixosConfigurations = {
-			eduardo = lib.nixosSystem {
-				inherit system;
-				modules = [ eduardo/os/configuration.nix ];
-			};
-			laptop = lib.nixosSystem {
-				inherit system;
-				modules = [ laptops/os/configuration.nix ];
-			};
-		};
-		hmConfig = {
-			eduardo = home-manager.lib.homeManagerConfiguration {
-				pkgs = import nixpkgs { inherit system; };
-				modules = [catppuccin.homeManagerModules.catppuccin  eduardo/homemanager/home.nix ];
-			};
-			laptop = home-manager.lib.homeManagerConfiguration {
-				pkgs = import nixpkgs { inherit system; };
-				modules = [catppuccin.homeManagerModules.catppuccin laptops/homemanager/home.nix ];
-			};
-		};
-	};
+    let
+      system = "x86_64-linux"; # change this if other architecture
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      lib = nixpkgs.lib;
+
+      k8sRole = builtins.getEnv "K8S_ROLE" || null;
+      ipAddress = builtins.getEnv "IP_ADDRESS" || null;
+
+    in {
+      nixosConfigurations = {
+        eduardo = lib.nixosSystem {
+          inherit system;
+          modules = [ eduardo/os/configuration.nix ];
+        };
+        laptop = lib.nixosSystem {
+          inherit system;
+          modules = [
+
+            laptops/os/configuration.nix
+            modules/kubernetes.nix
+            {
+              k8sRole = k8sRole;
+              ipAddress = ipAddress;
+            }
+          ];
+        };
+      };
+      hmConfig = {
+        eduardo = home-manager.lib.homeManagerConfiguration {
+          pkgs = import nixpkgs { inherit system; };
+          modules = [
+            catppuccin.homeManagerModules.catppuccin
+            eduardo/homemanager/home.nix
+          ];
+        };
+        laptop = home-manager.lib.homeManagerConfiguration {
+          pkgs = import nixpkgs { inherit system; };
+          modules = [
+            catppuccin.homeManagerModules.catppuccin
+            laptops/homemanager/home.nix
+          ];
+        };
+      };
+    };
 }
